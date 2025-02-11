730d6222282621929badae9a73c67dbb45155023
HzAjax <danil3000200@mail.ru>
2025-02-11 21:20:44 +0400

    initialize

---
+++ src/main/java/ru/volodin/SarComp/SarCompApplication.java
@@ -0,0 +1,13 @@
+package ru.volodin.SarComp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarCompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarCompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/volodin/SarComp/SarCompApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volodin.SarComp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarCompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
